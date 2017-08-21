1df78512bc113039733cb6fdc8792cd242d34e9b
Taoge <wenter.wu@gmail.com>
2017-08-21 14:27:45 +0800

    > fix event

--- daovoice/service/event.py
+++ daovoice/service/event.py
@@ -1,9 +1,9 @@
 from base_service import BaseService
 from daovoice import models
+from daovoice.api_operations import Save
 
 
-class Event(BaseService):
+class Event(BaseService, Save):
     @property
     def resource_class(self):
         return models.Event
-
